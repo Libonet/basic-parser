1eb312252bf2a223292d56b76525fd153187e21c
Santiago <santiago.libonati@gmail.com>
2023-07-26 19:56:31 -0400

    implementada la funcion para armar el trie del diccionario

--- src/lib/main.h
+++ src/lib/main.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include "lib/trie.h"
 
-TRIE trieDesdeArchivo(FILE archivo);
+TRIE trieDesdeArchivo(FILE* fp);
--- src/main.c
+++ src/main.c
@@ -2,8 +2,28 @@
 #include "lib/trie.h"
 #include "lib/main.h"
 
-TRIE trieDesdeArchivo(FILE archivo){
+// todas las palabras deben estar seguidas de un \n para funcionar
+TRIE trieDesdeArchivo(FILE* fp){
+    char c;
+    TRIE arbol = crearTrie();
+    TRIE aux = arbol;
+    
+    while(!feof(fp)){
+        c = fgetc(fp);
+        if (c == '\r')
+            continue;
+        if (c == '\n'){
+            aux->esFinal = 1;
+            aux = arbol;
+        }
+        else{
+            aux = trieApuntarHijo(aux, c);
+            if (aux == NULL)
+                aux = crearTrie();
+        }
+    }
 
+    return arbol;
 }
 
 // Se ingresa el path a los archivos en orden: "entrada", "salida, diccionario"
@@ -13,8 +33,17 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    FILE *fp = fopen(argv[1], "r");
-    TRIE trie = trieDesdeArchivo(fp);
+    FILE *fp = fopen(argv[3], "r"); // leemos el diccionario
+    TRIE diccionario = trieDesdeArchivo(fp);
+    fclose(fp);
+
+    fp = fopen(argv[1], "r"); // leemos la entrada
+
+    fclose(fp);
+
+    fp = fopen(argv[2], "w"); // guardamos en la salida
+
+    fclose(fp);
 
     return 0;
 }
