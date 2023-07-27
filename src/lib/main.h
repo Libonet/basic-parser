8ff8efb3714de089c030d01cb2807526262e210e
Santiago <santiago.libonati@gmail.com>
2023-07-26 20:23:18 -0400

    Documentadas un par de funciones

--- src/lib/main.h
+++ src/lib/main.h
@@ -1,4 +1,14 @@
-#include <stdio.h>
-#include "lib/trie.h"
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include "trie.h"
+
+// devuelve un trie armado con las palabras del archivo
 TRIE trieDesdeArchivo(FILE* fp);
+
+/*utilizando el diccionario, parsea las frases de la
+ entrada y las guarda bien separadas en la salida*/
+void parsear(FILE* fp1, FILE* fp2, TRIE diccionario);
+
+#endif /*  MAIN_H  */
