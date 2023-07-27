57ad0674635893df4eb06e9e577deadb902ded85
Santiago <santiago.libonati@gmail.com>
2023-07-27 19:36:20 -0400

    movido "trieDesdeArchivo" a trie.c

--- src/lib/main.h
+++ src/lib/main.h
@@ -4,9 +4,6 @@
 #include <stdio.h>
 #include "trie.h"
 
-// devuelve un trie armado con las palabras del archivo
-TRIE trieDesdeArchivo(FILE* fp);
-
 /*utilizando el diccionario, parsea las frases de la
  entrada y las guarda bien separadas en la salida*/
 void parsear(FILE* fp1, FILE* fp2, TRIE diccionario);
