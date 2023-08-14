995057b42c04d4711335d58db2105d156c2dbd70
Santiago <santiago.libonati@gmail.com>
2023-08-14 20:45:11 -0300

    implementada la optimizacion y refactorizadas un par de funciones

--- src/lib/trie.h
+++ src/lib/trie.h
@@ -3,29 +3,35 @@
 
 #include <stdio.h>
 
-// Para acceder a los hijos del TRIE usamos la funcion trieGet
-struct _TRIE
+// Para acceder a los hijos del TRIE usamos la funcion trieApuntarHijo
+struct _TRIENodo
 {
-    int esFinal;
-    struct _TRIE **hijos; // no consideramos la enie
-    // struct _TRIE **error; // posibles caminos si se encuentra un error
-    // int profundidad; // permite determinar cuantas letras desde el final son del prefijo y cuales son errores
+    int esFinal; // determina si el nodo es un estado de aceptacion
+    struct _TRIENodo **hijos;
+    struct _TRIENodo *padre; // referencia al padre necesaria para la optimizacion
+    struct _TRIENodo *sufijo; //apunta a un nodo del arbol que represente el camino del sufijo mas grande
+    int profundidad; // permite determinar cuantas letras desde el final son del prefijo y cuales son errores
 };
 
-typedef struct _TRIE* TRIE;
+typedef struct _TRIENodo* TRIE;
 
 // crea un nodo con esFinal en 0 y los punteros a hijos en nulo
 TRIE crearTrie();
 
+void destruirTrie(TRIE arbol);
+
 // funcion hash para acceder a los hijos del Trie
-int hash(char letra);
+int trieHash(char letra);
 
 // devuelve el nodo que le corresponde a la letra, o nulo si la letra no sigue al padre
 TRIE trieApuntarHijo(TRIE arbol, char letra);
 
-// todas las palabras deben estar seguidas de un \n para funcionar
-// devuelve un trie armado con las palabras del archivo
+/* devuelve un trie armado con las palabras del archivo
+ (todas las palabras deben estar seguidas de un \n)*/
 TRIE trieDesdeArchivo(FILE* fp);
+
+// inserta palabras terminadas con un \n
+void trieInsertarPalabra(TRIE arbol, char* palabra);
 
 // genera las conecciones entre nodos que permiten identificar si se esta leyendo otro prefijo al encontrar un error
 TRIE trieOptimizarDiccionario(TRIE arbol);
--- src/trie.c
+++ src/trie.c
@@ -2,56 +2,129 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include "trie.h"
-// #include "cola.h"
+#include "cola.h"
+
 
 TRIE crearTrie(){
-    TRIE nuevoTrie = malloc(sizeof(struct _TRIE));
+    TRIE nuevoTrie = malloc(sizeof(struct _TRIENodo));
 
     nuevoTrie->esFinal = 0;
-    nuevoTrie->hijos = malloc(sizeof(TRIE)*26);
-    for (int i=0; i<26; i++){
-        nuevoTrie->hijos[i] = NULL;
-    }
+    nuevoTrie->hijos = NULL;
+    nuevoTrie->padre = NULL;
+    nuevoTrie->sufijo = NULL;
+    nuevoTrie->profundidad = 0;
 
     return nuevoTrie;
 }
 
-int hash(char letra){ // asumimos que se ingresa una letra entre 'a' y 'z'
+void destruirTrie(TRIE arbol){
+    if (arbol == NULL)
+        return;
+
+    if (arbol->hijos != NULL){
+        for (int i=0; i<26; ++i){
+            destruirTrie(arbol->hijos[i]);
+        }
+    }
+    free(arbol);
+}
+
+// devuelve la posicion del diccionario correspondiente a la letra leida
+// (asumimos que se ingresa una letra entre 'a' y 'z')
+int trieHash(char letra){
     letra = tolower(letra);
 
     return letra-'a';
 }
 
 TRIE trieApuntarHijo(TRIE arbol, char letra){
-    int pos = hash(letra);
+    int pos = trieHash(letra);
 
     return arbol->hijos[pos];
 }
 
-// todas las palabras deben estar seguidas de un \n para funcionar
-TRIE trieDesdeArchivo(FILE* fp){
-    char c;
+TRIE trieDesdeArchivo(FILE* fpEntrada){
+    int profundidad = 0;
     TRIE arbol = crearTrie();
-    TRIE aux = arbol;
-    
-    while(!feof(fp)){
-        c = fgetc(fp);
-        if (c == '\r')
-            continue;
-        if (c == '\n'){
-            aux->esFinal = 1;
-            aux = arbol;
-        }
-        else{
-            aux = trieApuntarHijo(aux, c);
-            if (aux == NULL)
-                aux = crearTrie();
+    char *linea = NULL;
+    size_t largo = 0;
+    ssize_t nleido;
+
+    while(!feof(fpEntrada)){
+        if((nleido = getline(&linea, &largo, fpEntrada)) != -1){
+            printf("Retrieved line of length %zd:\n", nleido);
+
+            trieInsertarPalabra(arbol, linea);
         }
     }
+
+    free(linea);
 
     return arbol;
 }
 
-TRIE trieOptimizarDiccionario(TRIE arbol){
+// inserta palabras terminadas con un \n
+void trieInsertarPalabra(TRIE arbol, char* palabra){
+    int i, profundidad=0;
+    char letra;
+    TRIE auxArbol = arbol, hijo;
 
+    if (arbol == NULL)
+        return;
+
+    for (i=0; (letra = palabra[i])!='\n' && letra!='\0'; ++i){
+        if (letra == '\r')
+            continue;
+
+        if (auxArbol->hijos == NULL){
+            auxArbol->hijos = malloc(sizeof(TRIE)*26);
+            for (int i=0; i<26; i++){
+                auxArbol->hijos[i] = NULL;
+            }
+        }
+
+        hijo = trieApuntarHijo(auxArbol, letra);
+        profundidad++;
+        if (hijo == NULL)
+            hijo = crearTrie();
+            hijo->padre = auxArbol;
+            hijo->profundidad = profundidad;
+
+        auxArbol = hijo;
+    }
+}
+
+TRIE trieOptimizarDiccionario(TRIE arbol){
+    COLA cola = crearCola();
+    TRIE hijo, raiz=arbol, padre, sufijo;
+
+    if (arbol->hijos == NULL)
+        return arbol;
+
+    for (int i=0; i<26; ++i){
+        hijo = arbol->hijos[i];
+        if (hijo != NULL){
+            colaAgregarAlFinal(cola, hijo);
+            hijo->sufijo = raiz;
+        }
+    }
+
+    // recorremos "breadth first" usando la cola
+    while(cola->inicio!=NULL){
+        arbol = colaLeerInicio(cola);
+        colaEliminarInicio(cola);
+        for (int i=0; i<26; ++i){ // i=letra-'a' (0=a, 1=b, etc)
+            hijo = arbol->hijos[i];
+            if (hijo != NULL){
+                colaAgregarAlFinal(cola, hijo);
+
+                sufijo = padre->hijos[i];
+                while (sufijo==NULL && padre->sufijo!=NULL){
+                    sufijo = padre->hijos[i];
+                }
+            }
+        }
+    }
+
+    destruirCola(cola);
 }
