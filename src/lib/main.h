9de2f06f70ea60ff15979d80fd58f736b3084865
Santiago <santiago.libonati@gmail.com>
2023-08-14 20:43:42 -0300

    Progreso con el parseo de las frases

--- src/lib/main.h
+++ src/lib/main.h
@@ -4,8 +4,20 @@
 #include <stdio.h>
 #include "trie.h"
 
+typedef struct _SALIDA
+{
+    char* frase;
+    char** errores;
+    int cantErrores;
+} *SALIDA;
+
+// escribe en el archivo al que apunta "fpSalida" el contenido de "salidaParseada", formateandolo
+void fprintSalida(SALIDA salidaParseada, ssize_t largo, FILE* fpSalida);
+
+SALIDA parsearPalabra(char* linea, ssize_t* largo, TRIE diccionario);
+
 /*utilizando el diccionario, parsea las frases de la
  entrada y las guarda bien separadas en la salida*/
-void parsear(FILE* fp1, FILE* fp2, TRIE diccionario);
+void parsear(FILE* fpEntrada, FILE* fpSalida, TRIE diccionario);
 
 #endif /*  MAIN_H  */
--- src/main.c
+++ src/main.c
@@ -1,33 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "trie.h"
 #include "main.h"
 
-void parsear(FILE* fp1, FILE* fp2, TRIE diccionario){
-    
+void fprintSalida(SALIDA salidaParseada, ssize_t largo, FILE* fpSalida){
+    char letra;
+    char* frase = salidaParseada->frase;
+    if (salidaParseada->cantErrores==0){
+        fwrite(salidaParseada->frase, sizeof(char), largo, fpSalida);
+        
+    }
 }
 
-// Se ingresa el path a los archivos en orden: "entrada", "salida, diccionario"
+SALIDA parsearPalabra(char* linea, ssize_t* largo, TRIE diccionario){
+    SALIDA salidaParseada = malloc(sizeof(struct _SALIDA));
+    salidaParseada->errores = NULL;
+    salidaParseada->cantErrores = 0;
+
+
+
+    return salidaParseada;
+}
+
+void parsear(FILE* fpEntrada, FILE* fpSalida, TRIE diccionario){
+    char *linea;
+    size_t largo = 0;
+    ssize_t nleido;
+    SALIDA salidaParseada;
+
+    while(!feof(fpEntrada)){
+        linea = NULL;
+        if((nleido = getline(&linea, &largo, fpEntrada)) != -1) {
+            printf("Retrieved line of length %zd:\n", nleido);
+
+            salidaParseada = parsearPalabra(linea, &nleido, diccionario);
+
+            fprintSalida(salidaParseada, nleido, fpSalida);
+        }
+    }
+
+    free(linea);
+}
+
+// Se ingresa el path a los archivos en orden: diccionario", "entrada", "salida
 int main(int argc, char** argv){
     if (argc != 4){
         printf("Cantidad de argumentos invalida\n");
         return -1;
     }
 
-    FILE *fp1 = fopen(argv[3], "r"); // leemos el diccionario
-    TRIE diccionario = trieDesdeArchivo(fp1);
-    fclose(fp1);
+    FILE *fpDiccionario = fopen(argv[1], "r"); // leemos el diccionario
+    if (fpDiccionario == NULL){
+        printf("No se pudo leer el archivo\n");
+        exit(1);
+    }
+    TRIE diccionario = trieDesdeArchivo(fpDiccionario);
+    fclose(fpDiccionario);
 
-    // diccionario = trieOptimizarDiccionario(diccionario);
+    diccionario = trieOptimizarDiccionario(diccionario);
 
-    fp1 = fopen(argv[1], "r"); // leemos la entrada
-    FILE *fp2 = fopen(argv[2], "w"); // y guardamos en la salida al mismo tiempo
+    FILE *fpEntrada = fopen(argv[2], "r"); // leemos la entrada
+    if (fpEntrada == NULL){
+        printf("No se pudo leer el archivo\n");
+        destruirTrie(diccionario);
+        exit(1);
+    }
+    FILE *fpSalida = fopen(argv[3], "w"); // y guardamos en la salida al mismo tiempo
     
-    parsear(fp1, fp2, diccionario);
+    parsear(fpEntrada, fpSalida, diccionario);
 
-    fclose(fp1);
-    fclose(fp2);
+    fclose(fpEntrada);
+    fclose(fpSalida);
 
-
+    destruirTrie(diccionario);
 
     return 0;
 }
